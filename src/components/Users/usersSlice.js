531a00fb51f9eefd1f7063c6a794685b84ba45b2
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-07-31 08:41:39 -0400

    components

---
+++ src/components/Users/usersSlice.js
@@ -0,0 +1,14 @@
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
+import { apiSlice } from "../../app/api/apiSlice";
+
+const usersAdapter = createEntityAdapter({});
+const initialState = usersAdapter.getInitialState();
+export const extendedusersSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => "/users",
+    }),
+  }),
+});
+
+export const { useGetUsersQuery } = extendedusersSlice;
